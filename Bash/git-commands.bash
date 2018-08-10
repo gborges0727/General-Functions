0b2707b54e1ded3a5ca85b39101634be6274f769
Borges <gborges@borges173775gal.ent.gartner.com>
2018-08-10 09:16:47 -0400

    Add some commonly referenced git functions

---
+++ Bash/git-commands.bash
@@ -0,0 +1,22 @@
+# Undo recent commits
+git reset --soft HEAD^   # Most recent commit
+git reset --soft HEAD~x  # Go back 'x' commits
+git reset --hard HEAD^   # Undo most recent commit AND remove changes
+
+# Undo a 'git add'
+git reset <file> # Specific file
+git reset        # All files
+
+# Remove all commits in repository
+# -- WARNING -- Will delete all history PERMANENTLY
+git checkout --orphan latest_branch
+git add -A 
+git commit -am "commit message"
+git branch -D master 
+git branch -m master 
+git push -f origin master 
+
+# Sources: 
+# https://stackoverflow.com/questions/13716658/how-to-delete-all-commit-history-in-github
+# https://stackoverflow.com/questions/348170/how-to-undo-git-add-before-commit
+# https://stackoverflow.com/questions/2845731/how-to-uncommit-my-last-commit-in-git
